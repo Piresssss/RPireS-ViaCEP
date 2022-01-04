045fec3fea89afb0d35112681be27b4ae49760f9
Piresssss <faelpiressilva@hotmail.com>
2022-01-03 21:19:26 -0300

    mento erros e requisição

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,17 +1,14 @@
 import Address from "../models/address.js";
+import * as addressService from "../services/address-service.js";
 
 function State() {
-
     this.address = new Address();
-
     this.btnSave = null;
     this.btnClear = null;
-
     this.inputCep = null;
     this.inputStreet = null;
     this.inputNumber = null;
     this.inputCity = null;
-
     this.errorCep = null;
     this.errorNumber = null;
 }
@@ -23,14 +20,35 @@ export function init() {
     state.inputStreet = document.forms.newAddress.street;
     state.inputNumber = document.forms.newAddress.number;
     state.inputCity = document.forms.newAddress.city;
-
     state.btnSave = document.forms.newAddress.btnSave;
     state.btnClear = document.forms.newAddress.btnClear;
-
     state.errorCep = document.querySelector('[data-error="cep"]');
     state.errorNumber = document.querySelector('[data-error="number"]');
-
     state.inputNumber.addEventListener('change', handleInputNumberChange);
+    state.btnClear.addEventListener('click', handleBtnClearClick);
+    state.btnSave.addEventListener('click', handleBtnSaveClick);
+    state.inputCep.addEventListener('change', handleInputCepChange);
+}
+
+async function handleInputCepChange(event) {
+    const cep = event.target.value;
+    try {
+        const address = await addressService.findByCep(cep);
+        state.inputCity.value = address.city;
+        state.inputStreet.value = address.street;
+        state.address = address;
+        setFormError("cep", "");
+        state.inputNumber.focus();
+    } catch (e) {
+        state.inputStreet.value = "";
+        state.inputCity.value = "";
+        setFormError("cep", "Informe um CEP válido");
+    }
+}
+
+async function handleBtnSaveClick(event) {
+    event.preventDefault();
+    console.log(event.target);
 }
 
 function handleInputNumberChange(event) {
@@ -39,6 +57,21 @@ function handleInputNumberChange(event) {
     } else {
         setFormError("number", "");
     }
+}
+
+function handleBtnClearClick(event) {
+    event.preventDefault();
+    clearForm();
+}
+
+function clearForm() {
+    state.inputCep.value = "";
+    state.inputCity.value = "";
+    state.inputNumber.value = "";
+    state.inputStreet.value = "";
+    setFormError("cep", "");
+    setFormError("number", "");
+    state.inputCep.focus();
 }
 
 function setFormError(key, value) {
